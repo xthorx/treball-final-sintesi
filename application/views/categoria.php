23d492cfbfbfcfc46f8003f6ee5ba36d8e9e054b
xthorx <a.boladeres.fabregat@gmail.com>
2021-05-08 14:13:07 +0200

    nous canvis

--- application/views/categoria.php
+++ application/views/categoria.php
@@ -1,7 +1,22 @@
 <h1 class="text-center mb-5"><u><?php echo $titleMain;?></u></h1>
 
 
+
+
+
 <?php foreach($recursos_categoria as $recurs){ ?>
+    <div class="bg-light p-3 mb-2 container">
+        <div class="row text-center">
+            <div class="col-1"><p class="m-0 p-0">ID: <?php echo $recurs->id?></p></div>
+            <div class="col-5"><p class="m-0 p-0">Titol: <b><?php echo $recurs->titol?></b></p></div>
+            <div class="col-3"><p class="m-0 p-0">Categoria: <b><?php echo $rec_categoria?></b></p></div>
+            <div class="col-3"><p class="m-0 p-0">Autor: <b><?php echo $rec_autor?></b></p></div>
+        </div>
+    </div>
+<?php } ?>
+
+
+<!-- <?php foreach($recursos_categoria as $recurs){ ?>
     <div class="bg-primary p-3 mb-2">
 
         <h2><?php echo $recurs->titol?></h2>
@@ -13,4 +28,4 @@
 
     
 
-<?php } ?>
+<?php } ?> -->
--- application/views/recursos_llistat.php
+++ application/views/recursos_llistat.php
@@ -17,26 +17,15 @@
 
 <?php foreach($recursos_categoria as $recurs){ ?>
     <div class="bg-light p-3 mb-2 container">
-
         <div class="row text-center">
-
             <div class="col-1"><p class="m-0 p-0">ID: <?php echo $recurs->id?></p></div>
             <div class="col-5"><p class="m-0 p-0">Titol: <b><?php echo $recurs->titol?></b></p></div>
             <div class="col-2"><p class="m-0 p-0">Categoria: <b><?php echo $rec_categoria[$recurs->id]?></b></p></div>
             <div class="col-2"><p class="m-0 p-0">Autor: <b><?php echo $rec_autor[$recurs->id]?></b></p></div>
-
             <div class="col-2">
                 <a href="<?php echo base_url("/recursos/editar/" . $recurs->id)?>" class="btn btn-primary">Editar</a>
                 <a href="<?php echo base_url("/recursos/borrar/" . $recurs->id)?>" class="btn btn-danger">Borrar</a>
             </div>
-
         </div>
-
-        
-
     </div>
-
-
-    
-
 <?php } ?>
