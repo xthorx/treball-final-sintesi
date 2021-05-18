6c4885800269c75af7f231c914f5ba4a572a9f28
xthorx <a.boladeres.fabregat@gmail.com>
2021-05-18 13:33:32 +0200

    administrador canvis

--- application/models/model_administrador.php
+++ application/models/model_administrador.php
@@ -12,9 +12,14 @@ class model_administrador  extends CI_Model
 
 
     public function select_all_users(){
-        $sql = "SELECT * FROM users";
+        $sql = "SELECT users.id, users.username, users.email, groups.description, users.active, users.first_name, users.last_name, users.phone FROM `users`
+        INNER JOIN `users_groups` ON `users`.`id` = `users_groups`.`user_id`
+        INNER JOIN `groups` ON `users_groups`.`group_id` = `groups`.`id`
+        GROUP BY users.id";
+
         $query = $this->db->query($sql);
         return $query->result();
+
     }
 
     
--- application/views/admin_usuaris.php
+++ application/views/admin_usuaris.php
@@ -12,6 +12,7 @@
             <tr>
                 <th>Usuari</th>
                 <th>Correu electrònic</th>
+                <th>Tipus usuari</th>
                 <th>Actiu</th>
                 <th>Nom</th>
                 <th>Cognoms</th>
@@ -22,15 +23,16 @@
         <tbody>
             <?php foreach($infoUsers as $user){ ?>
                 <tr>
-                    <td><?php echo $user->username ?></td>
-                    <td><?php echo $user->email ?></td>
-                    <td><?php echo $user->active ?></td>
-                    <td><?php echo $user->first_name ?></td>
-                    <td><?php echo $user->last_name ?></td>
-                    <td><?php echo $user->phone ?></td>
+                    <td id="user<?php echo $user->id ?>"><?php echo $user->username ?></td>
+                    <td id="email<?php echo $user->id ?>"><?php echo $user->email ?></td>
+                    <td id="desc<?php echo $user->id ?>"><?php echo $user->description ?></td>
+                    <td id="act<?php echo $user->id ?>"><?php echo $user->active ?></td>
+                    <td id="fname<?php echo $user->id ?>"><?php echo $user->first_name ?></td>
+                    <td id="lname<?php echo $user->id ?>"><?php echo $user->last_name ?></td>
+                    <td id="phone<?php echo $user->id ?>"><?php echo $user->phone ?></td>
                     <td>
-                        <button class="btn btn-primary">Editar</button>
-                        <button class="btn btn-danger">Borrar</button>
+                        <button class="btn btn-primary" id="editar<?php echo $user->id ?>" onclick="editarCasella(<?php echo $user->id ?>)">Editar</button>
+                        <button class="btn btn-danger" id="borrar<?php echo $user->id ?>">Borrar</button>
                     </td>
                 </tr>
             <?php } ?>
@@ -39,6 +41,7 @@
             <tr>
                 <th>Usuari</th>
                 <th>Correu electrònic</th>
+                <th>Tipus usuari</th>
                 <th>Actiu</th>
                 <th>Nom</th>
                 <th>Cognoms</th>
@@ -48,6 +51,79 @@
         </tfoot>
     </table>
 </div>
+
+
+
+<script>
+
+    var edicioCasella= 0;
+
+    function editarCasella(id){
+
+        console.log(id);
+
+        if(edicioCasella != 0){
+            inputToText(edicioCasella);
+        }
+
+        document.getElementById("user" + id).innerHTML= "<input type='text' id='inputuser"+id+"' class='form-control' onfocus='activarFormulari("+id+")' value='"+ document.getElementById("user" + id).innerHTML +"'>";
+        document.getElementById("email" + id).innerHTML= "<input type='text' id='inputemail"+id+"' class='form-control' onfocus='activarFormulari("+id+")' value='"+ document.getElementById("email" + id).innerHTML +"'>";
+        document.getElementById("desc" + id).innerHTML= "<input type='text' id='inputdesc"+id+"' class='form-control' onfocus='activarFormulari("+id+")' value='"+ document.getElementById("desc" + id).innerHTML +"'>";
+        document.getElementById("act" + id).innerHTML= "<input type='text' id='inputact"+id+"' class='form-control' onfocus='activarFormulari("+id+")' value='"+ document.getElementById("act" + id).innerHTML +"'>";
+        document.getElementById("fname" + id).innerHTML= "<input type='text' id='inputfname"+id+"' class='form-control' onfocus='activarFormulari("+id+")' value='"+ document.getElementById("fname" + id).innerHTML +"'>";
+        document.getElementById("lname" + id).innerHTML= "<input type='text' id='inputlname"+id+"' class='form-control' onfocus='activarFormulari("+id+")' value='"+ document.getElementById("lname" + id).innerHTML +"'>";
+        document.getElementById("phone" + id).innerHTML= "<input type='text' id='inputphone"+id+"' class='form-control' onfocus='activarFormulari("+id+")' value='"+ document.getElementById("phone" + id).innerHTML +"'>";
+
+        edicioCasella=id;
+
+    }
+
+    function inputToText(id){
+
+        document.getElementById("user" + id).innerHTML=document.getElementById("inputuser" + id).value;
+        document.getElementById("email" + id).innerHTML=document.getElementById("inputemail" + id).value;
+        document.getElementById("desc" + id).innerHTML=document.getElementById("inputdesc" + id).value;
+        document.getElementById("act" + id).innerHTML=document.getElementById("inputact" + id).value;
+        document.getElementById("fname" + id).innerHTML=document.getElementById("inputfname" + id).value;
+        document.getElementById("lname" + id).innerHTML=document.getElementById("inputlname" + id).value;
+        document.getElementById("phone" + id).innerHTML=document.getElementById("inputphone" + id).value;
+
+         
+    }
+
+
+    function activarFormulari(id){
+
+
+        document.getElementById("inputuser" + id).value;
+        document.getElementById("inputemail" + id).value;
+        document.getElementById("inputdesc" + id).value;
+        document.getElementById("inputact" + id).value;
+        document.getElementById("inputfname" + id).value;
+        document.getElementById("inputlname" + id).value;
+        document.getElementById("inputphone" + id).value;
+
+
+        document.getElementById("editar" + id).innerHTML= "Guardar";
+        document.getElementById("editar" + id).className = "btn btn-success";
+
+
+
+    }
+
+
+
+
+
+</script>
+
+
+
+
+
+
+
+
 
 
 <script>
